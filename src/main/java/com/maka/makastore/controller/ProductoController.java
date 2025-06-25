3772e5e40e7a2c81d758bd4f767dc1c38cd93275
KDMV <franco_ojeda@soy.sena.edu.co>
2025-06-24 22:15:25 -0500

    actualizacion commit del proyecto maka-store

--- src/main/java/com/maka/makastore/controller/ProductoController.java
+++ src/main/java/com/maka/makastore/controller/ProductoController.java
@@ -3,6 +3,9 @@ package com.maka.makastore.controller;
 import com.maka.makastore.model.Producto;
 import com.maka.makastore.repository.ProductoRepository;
 import org.springframework.web.bind.annotation.*;
+import java.lang.reflect.Field;
+import java.util.Map;
+import org.springframework.util.ReflectionUtils;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,6 +39,21 @@ public class ProductoController {
     public Producto actualizar(@PathVariable Long id, @RequestBody Producto p) {
         p.setIdProducto(id);
         return repo.save(p);
+    }
+
+    @PatchMapping("/{id}")
+    public Producto actualizarParcial(@PathVariable Long id, @RequestBody Map<String, Object> campos) {
+        Producto producto = repo.findById(id).orElseThrow();
+
+        campos.forEach((clave, valor) -> {
+            Field campo = ReflectionUtils.findField(Producto.class, clave);
+            if (campo != null) {
+                campo.setAccessible(true);
+                ReflectionUtils.setField(campo, producto, valor);
+            }
+        });
+
+        return repo.save(producto);
     }
 
     @DeleteMapping("/{id}")
